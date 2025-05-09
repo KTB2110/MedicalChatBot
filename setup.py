ec090fc2c835485e3322cb2eee91941a18c36a40
KTB2110 <70537643+KTB2110@users.noreply.github.com>
2025-05-09 14:15:16 +0530

    folder structure added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = "Gen-AI Medical Chatbot",
+    version= '0.0.1',
+    author= "Krishna Tej Bhat",
+    author_email= 'krishnatej.bhat@gmail.com',
+    packages= find_packages(),
+    install_requires= []
+)
